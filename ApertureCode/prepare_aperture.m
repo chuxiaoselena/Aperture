31a6c640c362e33568302f981a64edc7ee3318e6
chuxiaoselena <chuxiaoselena@gmail.com>
2017-01-17 14:14:56 -0500

    ammendment on prepare_aperture

--- ApertureCode/prepare_aperture.m
+++ ApertureCode/prepare_aperture.m
@@ -5,13 +5,16 @@ cachedir = conf.cachedir;
 
 clusters = learn_clusters(pos_train, pos_val, tsize);
 psize = [150 150];
+wt_dir = '../../dataset/patch/';
 
 %% validation data
+if 0
 label_val = derive_labels('val', clusters, pos_val, tsize);
 val_imdata = num2cell(pos_val);
 val_labels = num2cell(label_val);
-wt_dir = '../../dataset/patch/';
+
 write_patch(val_imdata, val_labels, wt_dir, psize, 'val');
+end
 
 %% generate dummy labels for negative images
 label_train = derive_labels('train', clusters, pos_train, tsize);
@@ -21,11 +24,11 @@ rng(0);     % reproduce results
 train_imdata = cat(1, num2cell(pos_train), num2cell(neg_train));
 train_labels = cat(1, num2cell(label_train), num2cell(dummy_label));
 
-psize = tsize * conf.step;
 % permute training items
-perm_idx = randperm(numel(train_imdata));
-train_imdata = train_imdata(perm_idx);
-train_labels = train_labels(perm_idx);
+perm_idx1 = randperm(numel(train_imdata));
+perm_idx2 = randperm(numel(train_imdata));
+train_imdata = cat(1, train_imdata(perm_idx1), train_imdata(perm_idx2));
+train_labels = cat(1, train_labels(perm_idx1), train_labels(perm_idx2));
 
-write_patch(train_imdata, train_labels, wt_dir, 'train');
+write_patch(train_imdata, train_labels, wt_dir,psize, 'train');
 
--- ApertureCode/write_patch.m
+++ ApertureCode/write_patch.m
@@ -5,11 +5,11 @@ if ~isdir([wt phase '/']), mkdir([wt phase '/']); end
 fid = fopen([wt phase '.txt'],'w');
 for i = 1:length(data)
     if mod(i, 100) == 0
-        fprintf(1, '%s %d/%d\n', i, length(data));
+        fprintf('%s %d/%d\n', phase, i, length(data));
     end
     cpatch = crop_patch(data{i}, label{i}, psize);
     if strcmp(phase,'train')
-        perm_idx = randperm(numel(train_imdata));
+        perm_idx = randperm(numel(cpatch));
         cpatch = cpatch(perm_idx);
     end
     for j = 1:length(cpatch)
