5d2b4e80834fe3cc6962c4f563443d46285f5059
chuxiaoselena <chuxiaoselena@gmail.com>
2017-01-20 20:40:38 -0500

    fix

--- Test.m
+++ Test.m
@@ -6,10 +6,11 @@ conf = global_conf();
 pa = conf.pa;
 p_no = numel(pa);
 cachedir = conf.cachedir;
-modeldir = '../../model/LSP_iter_11500.caffemodel';
+modeldir = '../../model/LSP_iter_115000.caffemodel';
 deploydir = 'proto/highdim/deploy.prototxt';
 use_gpu = 1; gpu_id = 1;
-net=matcaffe_init(use_gpu, model_def_file, model_file,gpu_id);
+
+% net=matcaffe_init(use_gpu, deploydir, modeldir,gpu_id);
 Apsize = [24 48 72 96 120, 144];
 pos_test = LSP_test_data();
 label = 1:26;
